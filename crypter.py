de45bc13e6bbce82d38ae2353837ad1eef97dbb6
KoShAkiy <80166034+KoShAkiy@users.noreply.github.com>
2022-07-30 17:42:42 +0600

    Update crypter.py

--- crypter.py
+++ crypter.py
@@ -1,26 +1,26 @@
 import os
 import pyAesCrypt
 
-def crypt_and_decrypt(crypt_or_decrypt,file):
+def crypt_and_decrypt(crypt_or_decrypt,file): # Функция для шифровки и расшифровки файлов
         password = input('Введи пароль для шифровки. В будущем он понадобится для раскодировки: ')
         buffer = 512*1024
         bezrashireniy= file.split('.')
         if crypt_or_not == 1:
             try:
-                pyAesCrypt.encryptFile(file, bezrashireniy[0] + '.word', password, buffer)
+                pyAesCrypt.encryptFile(file, bezrashireniy[0] + '.word', password, buffer) # Сюда передается 4 аргумента, сам файл, как он должен выглядеть после шифровки, пароль и размер буфера
                 
-            except FileNotFoundError and ValueError:
+            except FileNotFoundError and ValueError: # На случай ошибки
                 print('Не удалось сделать шифровку файла')
 
         elif crypt_or_not == 0:
             typefile = input('Напиши тип файла: ')
             try:
-                pyAesCrypt.decryptFile(file, bezrashireniy[0]+'.'+typefile, password, buffer)
+                pyAesCrypt.decryptFile(file, bezrashireniy[0]+'.'+typefile, password, buffer) # Тут все то же самое
 
             except FileNotFoundError and ValueError:
                 print('Дешифровка не вышла')
 
-        os.remove(file)
+        os.remove(file) # Удаление файла после шифровки или дешифровки
 
 
 
